c68edf6f42cd29668498800edd4ff768ffc76e64
darren <darrenteokc@gmail.com>
2023-04-21 15:10:59 +0800

    added marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,8 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN; // environment variable that we set up, this is defined when we build our appn through CICD, tells us where the prod appn is actually hosted, which we will tell the marketing remote to draw from
+const domain = process.env.PRODUCTION_DOMAIN; // environment variable that we set up, this is defined when we build our appn through CICD, tells us where the prod appn is actually hosted, which we will tell the marketing remote to draw from.
+// the PRODUCTION_DOMAIN secret will be the URL of the Cloudfront distribution. it will be pulled through the workflow yml file during build
 
 const prodConfig = {
     mode: 'production',
@@ -11,12 +12,13 @@ const prodConfig = {
         filename: '[name].[contenthash].js', // this ensures that whenever we build files for prod, all the files will use this as a template for how to name them. Done because of caching issues
         publicPath: '/container/latest/', // this is used anytime we have some part of Webpack that refers to a file built by Webpack. Eg. when our HTML plugin refers to a js file that has been created.
         // so whenever our HTML plugin starts to figure out script tags to add to the document, it will prepend them with the publicPath
+        // in this case, the remoteEntry.js file will prepend all the folder names
     },
     plugins: [
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js` // assuming the remoteEntry.js file is nested inside the domain/marketing/
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js` // assuming the remoteEntry.js file is nested inside the domain/marketing/
             },
             shared: packageJson.dependencies
         })
